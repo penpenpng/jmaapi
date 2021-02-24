2ae699ce48e9fe3bd4f396e3ca2034ca7c26f2ea
penpenpng <poman1638m@gmail.com>
2021-02-25 04:15:37 +0900

    Import consts

---
+++ src/type/api/consts.d.ts
@@ -0,0 +1,12 @@
+type JmaTelop = Record<
+  WeatherCode,
+  [string, string, string, WeatherLabel, WeatherLabelEn]
+>;
+type WeatherCode = number;
+type WeatherLabel = string;
+type WeatherLabelEn = string;
+
+// TODO: 本当に JmaOfficeCode か確かめる
+type JmaAreaFuken = Array<{ center: JmaOfficeCode; offices: JmaOfficeCode[] }>;
+
+type JmaWeekAreas = Record<string, string>;
